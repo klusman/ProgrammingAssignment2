026e69b618534e30192ed318a983b9417ff61956
joop <joopklusman@gmail.com>
2014-09-21 20:16:34 +0200

    R-code incomplete

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,33 @@
-## Put comments here that give an overall description of what your
+#### cachematrix.R ####
+#### part of ProgrammingAssignment2
+
+
+## Function   cacheSolve(matrix) 
+## does the same als solve(matrix)
+## it returns the inverse-matrix
+##
+## Function    makeCacheMatrix.
+## Out of signt cacheSolve(matrix) uses function makeCacheMatrix.
+## it stores and retrieves the last used matrix an with solve(matrix) calculated inverse.
+## if function cacheSolve is used two or more times at a row with the same matrix.
+## it shoud prevent solve(matrix) being use with the same matrix.
+## this could be time efficient with big matrixes
+
 ## functions do
 
-## Write a short comment describing this function
+
+## makeCacheMatrix  function
+## creates a special list containing a function to
+#  1 set the value of the matrix
+#  2 get the value of the matrix
+#  3 set the value of the inverse matrix
+#  4 get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+## still thinking about it
+        ## must look something like makeVector in into texst
+        
 }
 
 
