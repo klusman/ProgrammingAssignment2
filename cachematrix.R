49c5f2169508d57a3ff07050409b0c501dddec05
joop <joopklusman@gmail.com>
2014-09-21 21:07:48 +0200

    R-code test1

--- cachematrix.R
+++ cachematrix.R
@@ -23,16 +23,31 @@
 #  3 set the value of the inverse matrix
 #  4 get the value of the inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
-## still thinking about it
-        ## must look something like makeVector in into texst
-        
+makeCacheMatrix <- function(x) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## returns inverse matrix
+## if possible from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m) ) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
